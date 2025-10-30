1049472f5e8c465b61d10c5943832b62220f05e7
Vladimir Tolmachev <tolm_vl@hotmail.com>
2025-10-30 21:30:24 +0700

    remove stringstream

--- src/http/utils.cpp
+++ src/http/utils.cpp
@@ -159,10 +159,12 @@ std::string sha256(const std::string &input) {
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256(reinterpret_cast<const unsigned char *>(input.c_str()), input.length(), hash);
 
-    std::stringstream ss;
+    std::string output;
+    output.resize(SHA256_DIGEST_LENGTH * 2);
+
     for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i)
-        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
-    return ss.str();
+        std::snprintf(&output[i * 2], 3, "%02x", hash[i]);
+    return output;
 }
 
 std::vector<std::string> split(const std::string &values, char delimiter) {
