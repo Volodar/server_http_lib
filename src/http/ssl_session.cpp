71f6c7da059defd98036c39ea07735f86bdbd7db
Vladimir Tolmachev <tolm_vl@hotmail.com>
2026-01-21 21:16:26 +0700

    added get params to log

--- src/http/ssl_session.cpp
+++ src/http/ssl_session.cpp
@@ -18,7 +18,7 @@ void SslSession<SocketType>::process_request(std::string&& header_,
     Response response = Response404;
     std::string statusText = "OK";
     auto it = _endpoints->find(key);
-    log_debug << methodToStr(request.get_method()) << ": " << request.get_path();
+    log_debug << methodToStr(request.get_method()) << ": " << request.get_path() << "?" << request.get_params().to_string();
     if (it != _endpoints->end()) {
         try {
             auto sequire = it->second.second;
